812bf7b33cffa5a168c8a1099521fe35d7fa6ec4
shadowmancwb <chaves.paulo.br@gmail.com>
2020-05-19 09:19:42 -0300

    Added the concat file

---
+++ modules/examples/concat.go
@@ -0,0 +1,16 @@
+//Concatenation of variables
+
+package main
+
+import "fmt"
+
+func main() {
+	word := "Hello"
+	word2 := "Arch"
+
+	fmt.Println(word + word2)
+
+	fmt.Println(word, word2)
+
+	fmt.Println(word + " " + word2)
+}

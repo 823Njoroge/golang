2356d888e45ff2c0a95b20a57421b39b5c006240
shadowmancwb <chaves.paulo.br@gmail.com>
2020-05-19 10:08:10 -0300

    Added the example function file

--- modules/examples/functions.go
+++ modules/examples/functions.go
@@ -10,7 +10,16 @@ func sum(x int, y int) int {
 	return x + y
 }
 
+func calc(a int) (int, int) {
+	var square int = a * a
+	var cube int = a * a * a
+
+	return square, cube
+}
+
 func main() {
 
 	fmt.Println(sum(3, 4))
+
+	fmt.Println(calc(2))
 }

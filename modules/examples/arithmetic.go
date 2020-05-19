f049a1b370999fdb3bdd7b03688f2e31b913b6c4
shadowmancwb <chaves.paulo.br@gmail.com>
2020-05-19 11:55:27 -0300

    Added the arithmetic operations function file

--- modules/examples/arithmetic.go
+++ modules/examples/arithmetic.go
@@ -1,2 +1,30 @@
 // Arithmetic Operations in the Go Language (Mathematics in Golang + - / *%)
 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// +-*/
+	// %
+
+	var a float64 = 22
+	var b float64 = 8
+
+	var c int = 8
+	var d int = 3
+
+	fmt.Println("a + b =", a+b)
+
+	fmt.Println("a - b =", a-b)
+
+	fmt.Println("a * b =", a*b)
+
+	fmt.Println("a / b =", a/b)
+
+	fmt.Println("c % d =", c%d)
+
+}

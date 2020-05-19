e739ced61600c758ea88e3cbc60f188c3e43dea5
shadowmancwb <chaves.paulo.br@gmail.com>
2020-05-19 09:54:01 -0300

    Added the constants file

---
+++ modules/examples/constants.go
@@ -0,0 +1,17 @@
+// Constants in the Go / Golang Programming Language
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	// const Pi = 3.14
+	var number = math.Max(3, 5)
+
+	fmt.Println(number)
+
+}
